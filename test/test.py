c24bce5fde75a72686da870471c460321ed7d2b2
Jason Rauen <jason.rauen@gmail.com>
2020-05-25 12:55:12 -0400

    switch to time-based waiting. no more docker shenanigans.

--- test/test.py
+++ test/test.py
@@ -1,16 +1,22 @@
-import docker
+import requests
 from selenium import webdriver
+import sys
 import time
 
-print("Connect to docker service")
-client = docker.APIClient(base_url="unix://var/run/docker.sock")
-i = 0
-health = ""
-while health != "healthy" and i < 120:  # max two minute wait time
-    health = client.inspect_container("pyden-splunk-1")['State']['Health']['Status']
-    time.sleep(1)
+
+print("Waiting for Splunk to be ready")
+i, waiting = 0, True
+while waiting:
+    r = requests.get("http://pyden-splunk:8000")
+    if r.ok:
+        waiting = False
+    if i < 120:
+        print("Splunk did not come up within 2 minutes")
+        sys.exit(1)
     i += 1
-print("Beginning automated tests...")
+    time.sleep(1)
+
+print("Beginning automated tests")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
