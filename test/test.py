e54f8433a09057bc9ac8fd012f855517b0c0af15
Jason Rauen <jason.rauen@gmail.com>
2020-05-25 00:45:56 -0400

    most kinks worked out on compose waiting for splunk container to become healthy

--- test/test.py
+++ test/test.py
@@ -1,6 +1,15 @@
+import docker
 from selenium import webdriver
+import time
 
-
+print("Connect to docker service")
+client = docker.APIClient(base_url="unix://var/run/docker.sock")
+i = 0
+health = ""
+while health != "healthy" and i < 120:  # max two minute wait time
+    health = client.inspect_container("pyden-splunk-1")['State']['Health']['Status']
+    time.sleep(1)
+    i += 1
 print("Beginning automated tests...")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
