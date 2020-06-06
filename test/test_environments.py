95fe86dcd760285404f0994cbdad328e5304ea01
Jason Rauen <jason.rauen@gmail.com>
2020-06-05 23:43:33 -0400

    redid tests to run in order as compiling python several times is much slower than the time saved running in parallel. especially given gitlab shared runners are on a single core.

--- test/test_environments.py
+++ test/test_environments.py
@@ -1,29 +1,24 @@
 from .drivers import splunk
-import time
 
 
 def test_create_venv_success(browser):
     splunk_test = splunk.SplunkTest(browser, "create_venv_success")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| createdist version=3.6.0")
-    assert "Successfully compiled Python 3.6.0" in results
-    results = splunk_test.run_search("| createvenv name=py-env-0 version=3.6.0")
-    assert "Successfully created virtual environment py-env-0 using Python 3.6.0" in results
+    results = splunk_test.run_search("| createdist version=3.8.0")
+    assert "Successfully compiled Python 3.8.0" in results
+    results = splunk_test.run_search("| createvenv name=py-env-0 version=3.8.0")
+    assert "Successfully created virtual environment py-env-0 using Python 3.8.0" in results
 
 
 def test_create_venv_name_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "create_venv_name_exists")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| createdist version=3.6.1")
-    assert "Successfully compiled Python 3.6.1" in results
-    results = splunk_test.run_search("| createvenv name=py-env-1 version=3.6.1")
-    assert "Successfully created virtual environment py-env-1 using Python 3.6.1" in results
-    results = splunk_test.run_search("| createvenv name=py-env-1 version=3.6.1")
-    assert "Virtual environment name py-env-1 already exists" in results
+    results = splunk_test.run_search("| createvenv name=py-env-0 version=3.8.0")
+    assert "Virtual environment name py-env-0 already exists" in results
 
 
 def test_create_venv_version_not_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "create_venv_version_not_exists")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| createvenv name=py-env-2 version=3.6.2")
+    results = splunk_test.run_search("| createvenv name=py-env-1 version=3.8.1")
     assert "Python version not found in pyden.conf" in results
--- test/test_versions.py
+++ test/test_versions.py
@@ -12,26 +12,22 @@ def test_create_dist_success(browser):
 def test_create_dist_version_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "create_dist_version_exists")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| createdist version=3.7.1")
-    assert "Successfully compiled Python 3.7.1" in results
-    results = splunk_test.run_search("| createdist version=3.7.1")
+    results = splunk_test.run_search("| createdist version=3.7.0")
     assert "Version already exists" in results
 
 
 def test_delete_dist_success(browser):
     splunk_test = splunk.SplunkTest(browser, "delete_dist_success")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| createdist version=3.7.2")
-    assert "Successfully compiled Python 3.7.2" in results
-    results = splunk_test.run_search("| pydelete 3.7.2")
-    assert "Successfully deleted dist 3.7.2" in results
+    results = splunk_test.run_search("| pydelete 3.7.0")
+    assert "Successfully deleted dist 3.7.0" in results
 
 
 def test_delete_dist_version_not_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "delete_dist_version_not_exists")
     splunk_test.open_pyden_search()
-    results = splunk_test.run_search("| pydelete 3.7.3")
-    assert "Version or environment 3.7.3 not found in configuration" in results
+    results = splunk_test.run_search("| pydelete 3.7.0")
+    assert "Version or environment 3.7.0 not found in configuration" in results
 
 
 def test_create_dist_ui_success(browser):
@@ -49,13 +45,6 @@ def test_create_dist_ui_success(browser):
 def test_delete_dist_ui_success(browser):
     splunk_test = splunk.SplunkTest(browser, "delete_dist_ui_success")
     splunk_test.open_pyden_versions()
-    icon = browser.find_element_by_class_name("icon-plus-circle")
-    icon.click()
-    i = 0
-    while "icon-minus-circle" not in icon.get_attribute("class") and i < 300:
-        time.sleep(1)
-        i += 1
-    assert i < 300
     icon = browser.find_element_by_class_name("icon-minus-circle")
     icon.click()
     i = 0
