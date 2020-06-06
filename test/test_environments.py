e3ff6cf6fef9346c578bc98529312a44d26afa48
Jason Rauen <jason.rauen@gmail.com>
2020-06-06 18:07:21 -0400

    ok, looks like maybe the issue was not sufficiently rebuilding the docker images frequently enough. setting to rebuild every time and removed extraneous test commands.

--- test/test_environments.py
+++ test/test_environments.py
@@ -6,7 +6,6 @@ def test_create_venv_success(browser):
     splunk_test.open_pyden_search()
     results = splunk_test.run_search("| createdist version=3.8.0")
     assert "Successfully compiled Python 3.8.0" in results
-    splunk_test.run_search("| getvenvs")
     results = splunk_test.run_search("| createvenv name=py-env-0 version=3.8.0")
     assert "Successfully created virtual environment py-env-0 using Python 3.8.0" in results
 
@@ -14,16 +13,12 @@ def test_create_venv_success(browser):
 def test_create_venv_name_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "create_venv_name_exists")
     splunk_test.open_pyden_search()
-    splunk_test.run_search("| getvenvs")
     results = splunk_test.run_search("| createvenv name=py-env-0 version=3.8.0")
     assert "Virtual environment name py-env-0 already exists" in results
-    splunk_test.run_search("| getvenvs")
 
 
 def test_create_venv_version_not_exists(browser):
     splunk_test = splunk.SplunkTest(browser, "create_venv_version_not_exists")
     splunk_test.open_pyden_search()
-    splunk_test.run_search("| getvenvs")
     results = splunk_test.run_search("| createvenv name=py-env-1 version=3.8.1")
     assert "Python version not found in pyden.conf" in results
-    splunk_test.run_search("| getvenvs")
