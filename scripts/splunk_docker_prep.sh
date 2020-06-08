a7679c2bc15134b48eb884c4b856803ac8461d8f
Jason Rauen <jason.rauen@gmail.com>
2020-06-08 12:56:37 -0400

    attempt to attach splunk logs as artifact of ci job

--- scripts/splunk_docker_prep.sh
+++ scripts/splunk_docker_prep.sh
@@ -2,4 +2,13 @@
 
 sudo apt-get update > /dev/null
 
-sudo apt-get -yq install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev > /dev/null
+sudo apt-get -yq install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev rsync lsyncd > /dev/null
+
+sudo mkdir -p /builds
+sudo touch /etc/rsyncd.conf
+sudo tee -a /etc/rsyncd.conf << 'EOF'
+[splunklogs]
+path = /builds
+read only = false
+EOF
+sudo lsyncd -rsync /opt/splunk/var/log/splunk splunklogs
