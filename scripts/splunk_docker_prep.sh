5d6e8a3141cdbb2cc401284ec4b61c759502b84c
Jason Rauen <jason.rauen@gmail.com>
2020-06-09 16:40:28 -0400

    remove uid nonsense

--- scripts/splunk_docker_prep.sh
+++ scripts/splunk_docker_prep.sh
@@ -3,6 +3,6 @@
 sudo apt-get update > /dev/null
 
 sudo apt-get -yq install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev > /dev/null
-
-sudo usermod -u 1000 splunk
-sudo groupmod -g 1000 splunk
+#
+#sudo usermod -u 1000 splunk
+#sudo groupmod -g 1000 splunk
